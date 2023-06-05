8992e368212980995410471f2f211e2f8ef866fb
Souto <matheus.souto@marinha.mil.br>
2023-06-05 13:53:10 -0300

    [BUILD]: Configura rota inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+// components
+import { Routes, Route } from 'react-router-dom'
+
 const App = () => {
   return(
     <>
-      ...
+      <div>
+        <Routes>
+          <Route path='/' element={<h1>Home</h1>} />
+        </Routes>
+      </div>
     </>
   )
 }
