d5626126f86bbad9b7d67146f24ae2bbe81422b0
Matheus Souto <emaildopaypal007@gmail.com>
2023-06-05 21:16:24 -0300

    [FEAT] Altera login page

--- src/pages/Login/LoginArea.jsx
+++ src/pages/Login/LoginArea.jsx
@@ -9,9 +9,14 @@ const LoginArea = () => {
           <label className="mt-4 text-slate-200" htmlFor="password">Senha</label>
           <input className="p-2 rounded bg-slate-400 outline-yellow-500 text-white text-lg" type="password" name="pasword" id="password" placeholder="Digite seu e-mail..." />
         </div>
+        <div className="flex justify-start pt-3 px-3">
+            <input className="w-4" type="checkbox" name="remember" id="remember" />
+            <span className="ml-1 text-white">Lembrar-me</span>
+        </div>
         <div className="mx-auto">
           <button className="mt-6 py-2 px-4 rounded text-white font-bold bg-yellow-500 hover:bg-transparent hover:border" type="submit">Entrar</button>
         </div>
+        <p className="text-center pt-3 text-white font-semibold">Não possui uma conta? <span className="text-yellow-500 font-bold cursor-pointer">Cadastre seu Delivery!</span></p>
       </div>
 
     </div>
