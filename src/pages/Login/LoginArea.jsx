03b746dd56cdd2fbe1c896cd4091797e1b5b9151
Matheus Souto <emaildopaypal007@gmail.com>
2023-06-05 21:07:44 -0300

    [FEAT]: Finaliza layout page login

--- src/pages/Login/LoginArea.jsx
+++ src/pages/Login/LoginArea.jsx
@@ -1,18 +1,21 @@
- const LoginArea = () => {
-    return(
-        <div className="container mx-auto flex flex-col justify-center items-center px-4 w-screen h-screen bg-slate-600">
-            <h1 className="text-3xl text-white font-bold text-center pt-4 sm:text-4xl">Login</h1>
-            <div className="flex flex-col w-full px-3 sm:max-w-lg">
-                <label className="mt-4 text-slate-200" htmlFor="email">E-mail</label>
-                <input className="p-2 rounded bg-slate-400 outline-none text-white" type="text" name="email" id="email" placeholder="Digite seu e-mail..." />
-                <label className="mt-4 text-slate-200" htmlFor="password">Senha</label>
-                <input className="p-2 rounded bg-slate-400 outline-none text-white" type="password" name="pasword" id="password" placeholder="Digite seu e-mail..." />
-            </div>
-            <div>
-                <button className="mt-6 py-2 px-4 rounded text-white font-bold bg-yellow-500" type="submit">Entrar</button>
-            </div>
+const LoginArea = () => {
+  return (
+    <div className="container mx-auto flex flex-col justify-center items-center px-4 w-screen h-screen bg-slate-600">
+      <div className="w-full flex flex-col justify-center bg-slate-900/40 py-14 px-5 max-w-lg shadow-lg shadow-black/50">
+        <div className="flex flex-col justify-center w-full px-3 sm:max-w-lg mx-auto">
+          <h1 className="text-3xl text-white font-bold text-center pt-4 sm:text-4xl">Login</h1>
+          <label className="mt-4 text-slate-200" htmlFor="email">E-mail</label>
+          <input className="p-2 rounded bg-slate-400 outline-yellow-500 text-white text-lg" type="text" name="email" id="email" placeholder="Digite seu e-mail..." />
+          <label className="mt-4 text-slate-200" htmlFor="password">Senha</label>
+          <input className="p-2 rounded bg-slate-400 outline-yellow-500 text-white text-lg" type="password" name="pasword" id="password" placeholder="Digite seu e-mail..." />
         </div>
-    )
- }
+        <div className="mx-auto">
+          <button className="mt-6 py-2 px-4 rounded text-white font-bold bg-yellow-500 hover:bg-transparent hover:border" type="submit">Entrar</button>
+        </div>
+      </div>
 
- export default LoginArea
+    </div>
+  )
+}
+
+export default LoginArea
