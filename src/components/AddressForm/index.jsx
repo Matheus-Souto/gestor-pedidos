94ea95cde74d39141e35e771046f56968d24e3d2
Souto <matheus.souto@marinha.mil.br>
2023-06-12 10:40:04 -0300

    [FEAT]: Implementa layout delivery e address

--- src/components/AddressForm/index.jsx
+++ src/components/AddressForm/index.jsx
@@ -1,6 +1,93 @@
 const AddressForm = () => {
     return(
-        <div>...</div>
+        <div className="flex flex-col">
+            <div className="sm:flex sm:justify-center sm:w-full">
+                <div className="flex flex-col justify-center w-full sm:max-w-lg mx-auto ">
+                    <label className="mt-4 text-slate-200" htmlFor="rua">Rua</label>
+                    <input
+                        className="p-2 rounded bg-slate-400 outline-yellow-500 text-white text-lg"
+                        type="text"
+                        name="rua"
+                        id="rua"
+                        placeholder="Digite sua rua..."
+                        required
+                    />
+                </div>
+                <div className="flex flex-col justify-center w-full sm:max-w-lg mx-auto sm:pl-3">
+                    <label className="mt-4 text-slate-200" htmlFor="bairro">Bairro</label>
+                    <input
+                        className="p-2 rounded bg-slate-400 outline-yellow-500 text-white text-lg"
+                        type="text"
+                        name="bairro"
+                        id="bairro"
+                        placeholder="Digite seu bairro..."
+                        required
+                    />
+                </div>
+            </div>
+            <div className="sm:flex sm:justify-center sm:w-full">
+                <div className="flex flex-col justify-center w-full sm:max-w-lg mx-auto">
+                    <label className="mt-4 text-slate-200" htmlFor="cidade">Cidade</label>
+                    <input
+                        className="p-2 rounded bg-slate-400 outline-yellow-500 text-white text-lg"
+                        type="text"
+                        name="cidade"
+                        id="cidade"
+                        placeholder="Digite sua cidade..."
+                        required
+                    />
+                </div>
+                <div className="flex flex-col justify-center w-full sm:max-w-lg mx-auto sm:pl-3">
+                    <label className="mt-4 text-slate-200" htmlFor="estado">Estado</label>
+                    <input
+                        className="p-2 rounded bg-slate-400 outline-yellow-500 text-white text-lg"
+                        type="text"
+                        name="estado"
+                        id="estado"
+                        placeholder="Digite seu estado..."
+                        required
+                    />
+                </div>
+            </div>
+            <div className="sm:flex sm:justify-center sm:w-full">
+                <div className="flex flex-col justify-center w-full sm:max-w-lg mx-auto">
+                    <label className="mt-4 text-slate-200" htmlFor="cep">CEP</label>
+                    <input
+                        className="p-2 rounded bg-slate-400 outline-yellow-500 text-white text-lg"
+                        type="text"
+                        name="cep"
+                        id="cep"
+                        placeholder="Digite seu CEP..."
+                        required
+                    />
+                </div>
+                <div className="flex flex-col justify-center w-full sm:max-w-lg mx-auto sm:pl-3">
+                    <label className="mt-4 text-slate-200" htmlFor="numero">Número</label>
+                    <input
+                        className="p-2 rounded bg-slate-400 outline-yellow-500 text-white text-lg"
+                        type="text"
+                        name="numero"
+                        id="numero"
+                        placeholder="Digite seu número..."
+                        required
+                    />
+                </div>
+            </div>
+            <div className="w-full sm:flex sm:justify-center sm:w-full">
+                <div className="flex flex-col justify-center w-full sm:max-w-lg mx-auto">
+                    <label className="mt-4 text-slate-200" htmlFor="complemento">Complemento</label>
+                    <input
+                        className="p-2 rounded bg-slate-400 outline-yellow-500 text-white text-lg"
+                        type="text"
+                        name="complemento"
+                        id="complemento"
+                        placeholder="Digite seu complemento..."
+                        required
+                    />
+                </div>
+            </div>
+
+        </div>
     )
 }
 
--- src/components/DeliveryForm/index.jsx
+++ src/components/DeliveryForm/index.jsx
@@ -1,6 +1,61 @@
 const DeliveryForm = () => {
     return(
-        <div className="">...</div>
+        <div className="flex flex-col">
+            <div className="sm:flex sm:justify-center sm:w-full">
+                <div className="flex flex-col justify-center w-full sm:max-w-lg mx-auto ">
+                    <label className="mt-4 text-slate-200" htmlFor="nomeRestaurante">Nome do Restaurante</label>
+                    <input
+                        className="p-2 rounded bg-slate-400 outline-yellow-500 text-white text-lg"
+                        type="text"
+                        name="nomeRestaurante"
+                        id="nomeRestaurante"
+                        placeholder="Digite o nome do Restaurante..."
+                        required
+                    />
+                </div>
+                <div className="flex flex-col justify-center w-full sm:max-w-lg mx-auto sm:pl-3">
+                    <label className="mt-4 text-slate-200" htmlFor="logo">Logo</label>
+                    <input
+                        className="p-2 rounded bg-slate-400 outline-yellow-500 text-white text-lg"
+                        type="text"
+                        name="logo"
+                        id="logo"
+                        placeholder="Upload Logo do Restaurante..."
+                        required
+                    />
+                </div>
+            </div>
+            <div className="sm:flex sm:justify-center sm:w-full">
+                <div className="flex flex-col justify-center w-full sm:max-w-lg mx-auto">
+                   <div className="flex flex-col justify-center items-center sm:mt-12 ">
+                   <label className="mt-4 text-slate-200" htmlFor="email">Tipo de Restaurante</label>
+                    <div className="text-white">
+                        <input type="checkbox" id="topping" name="topping" value="Paneer" /> Lanches
+                    </div>
+                    <div className="text-white">
+                        <input type="checkbox" id="topping" name="topping" value="Paneer" /> Japonesa
+                    </div>
+                    <div className="text-white">
+                        <input type="checkbox" id="topping" name="topping" value="Paneer" /> Pizzaria
+                    </div>
+                    <div className="text-white">
+                        <input type="checkbox" id="topping" name="topping" value="Paneer" /> Massas
+                    </div>
+                   </div>
+                </div>
+                <div className="flex flex-col justify-center w-full sm:max-w-lg mx-auto sm:pl-3">
+                    <label className="mt-4 text-slate-200" htmlFor="cpfcnpj">CPF/CNPJ</label>
+                    <input
+                        className="p-2 rounded bg-slate-400 outline-yellow-500 text-white text-lg"
+                        type="text"
+                        name="cpfcnpj"
+                        id="cpfcnpj"
+                        placeholder="Digite o CPF/CNPJ..."
+                        required
+                    />
+                </div>
+            </div>
+        </div>
     )
 }
 
